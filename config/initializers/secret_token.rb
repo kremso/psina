31e0efd48367359cc54d10b5827260851c5852b8
Tomas Kramar <kramar.tomas@gmail.com>
2012-08-23 12:31:58 +0200

    Application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+Psina::Application.config.secret_token = 'aa57a107fbbac3c1c117b3572df1297cb45e64e2b8eb3c40970669a0374c3a082dc749bdfa3650d1b7f5437586a2a05cb8fb9c756d07e940c2d5211c61b234cf'
